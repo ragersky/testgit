d8ff971fb223e106f2ce61dbef90c4daa45e9399
RistovSasko <54620223+ragersky@users.noreply.github.com>
2020-01-13 19:15:38 +0100

    Update Hello.cs

--- Hello.cs
+++ Hello.cs
@@ -1,8 +1,8 @@
-// Ime datoteke: Hello.cs
 public class Hello
 {
    public static void Main()
    {
-      System.Console.WriteLine("Hello, World!");
+	System.Console.WriteLine("Hello, World!");
+	System.Console.WriteLine("Hello, GitHub!");
    }
 }
